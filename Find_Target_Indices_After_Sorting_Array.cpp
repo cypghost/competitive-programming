222fcd4135c78d770844efd8e826c44998bb55e0
BIRUK M <102286732+cypghost@users.noreply.github.com>
2022-10-18 18:18:18 +0300

    Update Find_Target_Indices_After_Sorting_Array.cpp

--- Find_Target_Indices_After_Sorting_Array.cpp
+++ Find_Target_Indices_After_Sorting_Array.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
         vector<int> ans;
         
-        int index = 0;
-        int frequency = 0;
-        
         for(int i = 0; i < nums.size(); i++){
-            if(nums[i] < target)
-                index++;
-            else if(nums[i] == target)
-                frequency++;
-        }
-        
-        while(frequency > 0){
-            ans.push_back(index);
-            frequency--;
-            index++;
+           if(nums[i] == target)
+                ans.push_back(i);
         }
         return ans;
     }
